7a861a8f4b54259dcab41eb5abb1ed7406ed51cc
WasiliySoft_remote <wasiliysoft@ya.ru>
2022-04-07 16:09:34 +0500

    initial compose

---
+++ app/src/main/java/ru/wasiliysoft/simplemagazin/MainActivityCompose.kt
@@ -0,0 +1,90 @@
+package ru.wasiliysoft.simplemagazin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+import ru.wasiliysoft.simplemagazin.ui.theme.SimpleMagazinTheme
+
+class MainActivityCompose : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleMagazinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CombinedTab()
+                }
+
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun CombinedTab() {
+    val tabData = listOf(
+        "MUSIC" to Icons.Filled.Home,
+        "MARKET" to Icons.Filled.ShoppingCart,
+        "FILMS" to Icons.Filled.AccountBox,
+        "BOOKS" to Icons.Filled.Settings,
+    )
+    val pagerState = rememberPagerState(1)
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator()
+            }
+        ) {
+            tabData.forEachIndexed { index, pair ->
+                Tab(selected = tabIndex == index, onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }, text = {
+                    Text(text = pair.first)
+                }, icon = {
+                    Icon(imageVector = pair.second, contentDescription = null)
+                })
+            }
+        }
+        HorizontalPager(
+            count = 2,
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { index ->
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = tabData[index].first,
+                )
+            }
+        }
+    }
+}
