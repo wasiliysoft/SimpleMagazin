31c945929f18f4b2ee452622fa77c2d474c9c90f
WasiliySoft-Home <wasiliysoft@ya.ru>
2024-10-06 13:50:29 +0500

    fix card padding

--- app/src/main/java/ru/wasiliysoft/simplemagazin/ui/view/CardList.kt
+++ app/src/main/java/ru/wasiliysoft/simplemagazin/ui/view/CardList.kt
@@ -3,9 +3,7 @@ package ru.wasiliysoft.simplemagazin.ui.view
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,7 +15,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ru.wasiliysoft.simplemagazin.data.SimpleItem
@@ -77,9 +74,10 @@ fun ItemCard(
             onLongClick = { cardCombinedClickable.onLongClick(item) }
         )
     }
-    Card(colors = CardDefaults.cardColors(containerColor = color), modifier = m) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterStart) {
-            Text(item.title, modifier = Modifier.padding(8.dp))
-        }
+    Card(
+        colors = CardDefaults.cardColors(containerColor = color),
+        modifier = m
+    ) {
+        Text(item.title, modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp))
     }
 }
