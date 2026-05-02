b655aa3ece006c84344c02ba98e512ec2c4d4d94
WasiliySoft-Home <wasiliysoft@ya.ru>
2026-05-02 16:20:53 +0500

    use libs.versions.toml

---
+++ app/build.gradle.kts
@@ -0,0 +1,101 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+
+    namespace = "ru.wasiliysoft.simplemagazin"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+    signingConfigs {
+        create("config") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
+    defaultConfig {
+        applicationId = "ru.wasiliysoft.simplemagazin"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 6
+        versionName = "1.1.4"
+//        resourceConfigurations += ['en', 'ru']
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            applicationIdSuffix = ".debug"
+            signingConfig = signingConfigs.getByName("config")
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("config")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+kotlin {
+    compilerOptions {
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+dependencies {
+    implementation(libs.retrofit.gson)
+    implementation(libs.activity.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.datastore)
+    implementation(libs.material)
+
+    implementation(libs.activity.compose)
+//    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.9.4'
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.navigation.compose)
+
+    implementation(platform(libs.compose.bom))
+
+    implementation(libs.compose.runtime.livedata)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material.icons.core)
+    implementation(libs.compose.material.icons.extended.android)
+    implementation(libs.compose.ui.tooling.preview)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+}
